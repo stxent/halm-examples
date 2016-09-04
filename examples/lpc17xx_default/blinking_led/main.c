e162cde014c6cebd3b2c48b7782b9fb307e3c5ef
xent <alexdmitv@gmail.com>
2016-09-05 01:00:59 +0300

    Added examples, updated linker and build scripts.
    Silent mode in makefile replaced with directory printing suppression.
    Refactored linker scripts, fixed issue with the initialization of
    static variables.
    Added blinking LED example for LPC17xx.

---
+++ examples/lpc17xx_default/blinking_led/main.c
@@ -0,0 +1,65 @@
+/*
+ * main.c
+ * Copyright (C) 2016 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <stdbool.h>
+
+#include <halm/pin.h>
+#include <halm/platform/nxp/gptimer.h>
+/*----------------------------------------------------------------------------*/
+#define LED_PIN PIN(0, 22)
+/*----------------------------------------------------------------------------*/
+static struct GpTimerConfig timerConfig = {
+    .frequency = 1000,
+    .channel = 0
+};
+/*----------------------------------------------------------------------------*/
+static void onTimerOverflow(void *argument)
+{
+  bool * const event = argument;
+
+  *event = true;
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  struct Timer *timer;
+  struct Pin led;
+  bool event = false;
+
+  led = pinInit(LED_PIN);
+  pinOutput(led, 0);
+
+  timer = init(GpTimer, &timerConfig);
+  assert(timer);
+
+  timerSetOverflow(timer, 500);
+  timerCallback(timer, onTimerOverflow, &event);
+  timerSetEnabled(timer, true);
+
+  uint8_t ledValue = 0;
+
+  while (1)
+  {
+    while (!event)
+      barrier();
+
+    event = false;
+
+    pinWrite(led, ledValue);
+    ledValue ^= 0x01;
+  }
+
+  return 0;
+}
+/*----------------------------------------------------------------------------*/
+void __assert_func(const char *file __attribute__((unused)),
+    int line __attribute__((unused)),
+    const char *func __attribute__((unused)),
+    const char *expr __attribute__((unused)))
+{
+  while (1);
+}
