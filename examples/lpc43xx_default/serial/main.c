a591dcccdcef04a3737794b3dbad3d8e39a9235c
xent <alexdmitv@gmail.com>
2017-01-02 01:48:44 +0300

    Added examples for Serial and Spi on LPC43xx.

---
+++ examples/lpc43xx_default/serial/main.c
@@ -0,0 +1,137 @@
+/*
+ * main.c
+ * Copyright (C) 2017 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <halm/pin.h>
+#include <halm/platform/nxp/lpc43xx/clocking.h>
+#include <halm/platform/nxp/serial.h>
+/*----------------------------------------------------------------------------*/
+#define BUFFER_SIZE 64
+#define LED_PIN     PIN(PORT_6, 6)
+/*----------------------------------------------------------------------------*/
+static const struct SerialConfig serialConfig = {
+    .rate = 19200,
+    .rxLength = 64,
+    .txLength = 64,
+    .rx = PIN(2, 4),
+    .tx = PIN(2, 3),
+    .channel = 3
+};
+/*----------------------------------------------------------------------------*/
+static const struct ExternalOscConfig extOscConfig = {
+    .frequency = 12000000,
+    .bypass = false
+};
+
+static const struct PllConfig sysPllConfig = {
+    .multiplier = 20,
+    .divisor = 4,
+    .source = CLOCK_EXTERNAL
+};
+
+static const struct CommonClockConfig mainClkConfig = {
+    .source = CLOCK_PLL
+};
+
+static const struct CommonClockConfig initialClock = {
+    .source = CLOCK_INTERNAL
+};
+/*----------------------------------------------------------------------------*/
+static void setupClock()
+{
+  clockEnable(MainClock, &initialClock);
+
+  clockEnable(ExternalOsc, &extOscConfig);
+  while (!clockReady(ExternalOsc));
+
+  clockEnable(SystemPll, &sysPllConfig);
+  while (!clockReady(SystemPll));
+
+  clockEnable(Usart3Clock, &mainClkConfig);
+  while (!clockReady(Usart3Clock));
+
+  clockEnable(MainClock, &mainClkConfig);
+}
+/*----------------------------------------------------------------------------*/
+static void onSerialEvent(void *argument)
+{
+  *(bool *)argument = true;
+}
+/*----------------------------------------------------------------------------*/
+static void processInput(struct Interface *interface, const char *input,
+    size_t length)
+{
+  char buffer[16];
+
+  while (length)
+  {
+    const size_t chunkLength = length > sizeof(buffer) ?
+        sizeof(buffer) : length;
+
+    for (size_t index = 0; index < chunkLength; ++index)
+      buffer[index] = input[index] + 1;
+
+    size_t pending = chunkLength;
+    const char *bufferPointer = buffer;
+
+    while (pending)
+    {
+      const size_t bytesWritten = ifWrite(interface, buffer, chunkLength);
+
+      pending -= bytesWritten;
+      bufferPointer += bytesWritten;
+    }
+
+    length -= chunkLength;
+    input += chunkLength;
+  }
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  setupClock();
+
+  const struct Pin led = pinInit(LED_PIN);
+  pinOutput(led, false);
+
+  char buffer[BUFFER_SIZE];
+  bool event = false;
+
+  struct Interface * const serial = init(Serial, &serialConfig);
+  assert(serial);
+  ifCallback(serial, onSerialEvent, &event);
+
+  while (1)
+  {
+    while (!event)
+      barrier();
+    event = false;
+
+    size_t available;
+
+    if (ifGet(serial, IF_AVAILABLE, &available) == E_OK && available > 0)
+    {
+      size_t bytesRead;
+
+      pinSet(led);
+
+      while ((bytesRead = ifRead(serial, buffer, sizeof(buffer))))
+        processInput(serial, buffer, bytesRead);
+
+      pinReset(led);
+    }
+  }
+
+  return 0;
+}
+/*----------------------------------------------------------------------------*/
+void __assert_func(const char *file __attribute__((unused)),
+    int line __attribute__((unused)),
+    const char *func __attribute__((unused)),
+    const char *expr __attribute__((unused)))
+{
+  while (1);
+}
---
+++ examples/lpc43xx_default/spi/main.c
@@ -0,0 +1,154 @@
+/*
+ * main.c
+ * Copyright (C) 2017 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <halm/pin.h>
+#include <halm/platform/nxp/gptimer.h>
+#include <halm/platform/nxp/lpc43xx/clocking.h>
+#include <halm/platform/nxp/spi.h>
+#include <halm/platform/nxp/spi_dma.h>
+#include <xcore/memory.h>
+/*----------------------------------------------------------------------------*/
+#define CS_PIN  PIN(PORT_5, 0)
+#define LED_PIN PIN(PORT_6, 6)
+
+#define TEST_DMA
+#define TEST_ZEROCOPY
+
+#ifdef TEST_DMA
+#define SPI_CLASS SpiDma
+#else
+#define SPI_CLASS Spi
+#endif
+
+#define SPI_CHANNEL 1
+/*----------------------------------------------------------------------------*/
+static const struct GpTimerConfig timerConfig = {
+    .frequency = 1000,
+    .channel = 0
+};
+/*----------------------------------------------------------------------------*/
+#ifdef TEST_DMA
+static const struct SpiDmaConfig spiConfig[] = {
+    {
+        .rate = 400000,
+        .sck = PIN(PORT_3, 3),
+        .miso = PIN(PORT_3, 6),
+        .mosi = PIN(PORT_3, 8),
+        .dma = {0, 1},
+        .channel = 0,
+        .mode = 0
+    },
+    {
+        .rate = 400000,
+        .sck = PIN(PORT_CLK, 0),
+        .miso = PIN(PORT_0, 0),
+        .mosi = PIN(PORT_0, 1),
+        .dma = {3, 2},
+        .channel = 1,
+        .mode = 0
+    }
+};
+#else
+static const struct SpiConfig spiConfig[] = {
+    {
+        .rate = 400000,
+        .sck = PIN(PORT_3, 3),
+        .miso = PIN(PORT_3, 6),
+        .mosi = PIN(PORT_3, 8),
+        .channel = 0,
+        .mode = 0
+    },
+    {
+        .rate = 400000,
+        .sck = PIN(PORT_CLK, 0),
+        .miso = PIN(PORT_0, 0),
+        .mosi = PIN(PORT_0, 1),
+        .channel = 1,
+        .mode = 0
+    }
+};
+#endif
+/*----------------------------------------------------------------------------*/
+static const struct CommonClockConfig initialClock = {
+    .source = CLOCK_INTERNAL
+};
+/*----------------------------------------------------------------------------*/
+static void setupClock()
+{
+  clockEnable(MainClock, &initialClock);
+}
+/*----------------------------------------------------------------------------*/
+#ifdef TEST_ZEROCOPY
+static void onTransferCompleted(void *argument)
+{
+  ++(*(unsigned int *)argument);
+}
+#endif
+/*----------------------------------------------------------------------------*/
+static void onTimerOverflow(void *argument)
+{
+  *(bool *)argument = true;
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  setupClock();
+
+  const struct Pin led = pinInit(LED_PIN);
+  pinOutput(led, false);
+
+  const struct Pin cs = pinInit(CS_PIN);
+  pinOutput(cs, true);
+
+  struct Interface * const spi = init(SPI_CLASS, &spiConfig[SPI_CHANNEL]);
+  assert(spi);
+
+  static const uint32_t desiredRate = 200000;
+  enum result res;
+
+  res = ifSet(spi, IF_RATE, &desiredRate);
+  assert(res == E_OK);
+
+  struct Timer * const timer = init(GpTimer, &timerConfig);
+  assert(timer);
+  timerSetOverflow(timer, 250);
+
+  unsigned int value = 0;
+  bool event = false;
+
+#ifdef TEST_ZEROCOPY
+  res = ifSet(spi, IF_ZEROCOPY, 0);
+  assert(res == E_OK);
+  res = ifCallback(spi, onTransferCompleted, &value);
+  assert(res == E_OK);
+#endif
+
+  (void)res; /* Suppress warning */
+
+  timerCallback(timer, onTimerOverflow, &event);
+  timerSetEnabled(timer, true);
+  pinReset(cs);
+
+  while (1)
+  {
+    while (!event)
+      barrier();
+    event = false;
+
+    const uint32_t buffer = toBigEndian32(value);
+
+    pinSet(led);
+    ifWrite(spi, &buffer, sizeof(buffer));
+    pinReset(led);
+
+#ifndef TEST_ZEROCOPY
+    ++value;
+#endif
+  }
+
+  return 0;
+}
