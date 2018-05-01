a89be9723ac8846bda653dd5cbfe9c5284037ad5
xent <alexdmitv@gmail.com>
2018-05-02 01:43:18 +0300

    Updated libraries, added examples for BOD and RIT.

---
+++ examples/lpc17xx_default/bod/main.c
@@ -0,0 +1,41 @@
+/*
+ * lpc17xx_default/bod/main.c
+ * Copyright (C) 2018 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <halm/pin.h>
+#include <halm/platform/nxp/bod.h>
+/*----------------------------------------------------------------------------*/
+#define LED_PIN_1 PIN(1, 8)
+#define LED_PIN_2 PIN(1, 9)
+/*----------------------------------------------------------------------------*/
+static const struct BodConfig bodConfig = {
+    .eventLevel = BOD_EVENT_2V2,
+    .resetLevel = BOD_RESET_DISABLED
+};
+/*----------------------------------------------------------------------------*/
+static void onBodEvent(void *argument)
+{
+  struct Pin * const leds = argument;
+
+  pinSet(leds[0]);
+  pinToggle(leds[1]);
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  struct Pin leds[2] = {
+      pinInit(LED_PIN_1),
+      pinInit(LED_PIN_2)
+  };
+
+  struct Interrupt * const bod = init(Bod, &bodConfig);
+  assert(bod);
+  interruptSetCallback(bod, onBodEvent, leds);
+  interruptEnable(bod);
+
+  while (1);
+  return 0;
+}
---
+++ examples/lpc17xx_default/rit/main.c
@@ -0,0 +1,41 @@
+/*
+ * lpc17xx_default/rit/main.c
+ * Copyright (C) 2018 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <halm/platform/nxp/rit.h>
+#include <halm/pin.h>
+/*----------------------------------------------------------------------------*/
+#define LED_PIN PIN(1, 8)
+/*----------------------------------------------------------------------------*/
+static void onTimerOverflow(void *argument)
+{
+  *(bool *)argument = true;
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  const struct Pin led = pinInit(LED_PIN);
+  pinOutput(led, true);
+
+  struct Timer * const timer = init(Rit, 0);
+  assert(timer);
+  timerSetOverflow(timer, timerGetFrequency(timer) / 2);
+
+  bool event = false;
+  timerSetCallback(timer, onTimerOverflow, &event);
+  timerEnable(timer);
+
+  while (1)
+  {
+    while (!event)
+      barrier();
+    event = false;
+
+    pinToggle(led);
+  }
+
+  return 0;
+}
