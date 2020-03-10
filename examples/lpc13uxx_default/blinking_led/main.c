bc425372bae84b7af72b522fc7faf6770855c8d2
xent <alexdmitv@gmail.com>
2020-03-11 01:26:27 +0300

    Added examples for LPC13Uxx series.

---
+++ examples/lpc13uxx_default/blinking_led/main.c
@@ -0,0 +1,46 @@
+/*
+ * lpc13uxx_default/blinking_led/main.c
+ * Copyright (C) 2020 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <halm/pin.h>
+#include <halm/platform/nxp/gptimer.h>
+/*----------------------------------------------------------------------------*/
+#define LED_PIN PIN(1, 13)
+/*----------------------------------------------------------------------------*/
+static const struct GpTimerConfig timerConfig = {
+    .frequency = 1000,
+    .channel = GPTIMER_CT32B0
+};
+/*----------------------------------------------------------------------------*/
+static void onTimerOverflow(void *argument)
+{
+  *(bool *)argument = true;
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  const struct Pin led = pinInit(LED_PIN);
+  pinOutput(led, true);
+
+  struct Timer * const timer = init(GpTimer, &timerConfig);
+  assert(timer);
+  timerSetOverflow(timer, 500);
+
+  bool event = false;
+  timerSetCallback(timer, onTimerOverflow, &event);
+  timerEnable(timer);
+
+  while (1)
+  {
+    while (!event)
+      barrier();
+    event = false;
+
+    pinToggle(led);
+  }
+
+  return 0;
+}
---
+++ examples/lpc13uxx_default/serial/main.c
@@ -0,0 +1,78 @@
+/*
+ * lpc13uxx_default/serial/main.c
+ * Copyright (C) 2020 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <assert.h>
+#include <halm/pin.h>
+#include <halm/platform/nxp/serial.h>
+#include <halm/platform/nxp/lpc13xx/clocking.h> // XXX
+/*----------------------------------------------------------------------------*/
+#define BUFFER_SIZE 64
+#define LED_PIN     PIN(1, 13)
+/*----------------------------------------------------------------------------*/
+static const struct SerialConfig serialConfig = {
+    .rate = 19200,
+    .rxLength = 64,
+    .txLength = 64,
+    .rx = PIN(0, 18),
+    .tx = PIN(0, 19),
+    .channel = 0
+};
+/*----------------------------------------------------------------------------*/
+static void onSerialEvent(void *argument)
+{
+  *(bool *)argument = true;
+}
+/*----------------------------------------------------------------------------*/
+static void processInput(struct Interface *interface, char *buffer,
+    size_t length)
+{
+  for (size_t index = 0; index < length; ++index)
+    ++buffer[index];
+
+  while (length)
+  {
+    const size_t bytesWritten = ifWrite(interface, buffer, length);
+
+    length -= bytesWritten;
+    buffer += bytesWritten;
+  }
+}
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+  const struct Pin led = pinInit(LED_PIN);
+  pinOutput(led, false);
+
+  bool event = false;
+
+  struct Interface * const serial = init(Serial, &serialConfig);
+  assert(serial);
+  ifSetCallback(serial, onSerialEvent, &event);
+
+  while (1)
+  {
+    while (!event)
+      barrier();
+    event = false;
+
+    size_t available;
+
+    if (ifGetParam(serial, IF_AVAILABLE, &available) == E_OK && available > 0)
+    {
+      char buffer[BUFFER_SIZE];
+      size_t bytesRead;
+
+      pinSet(led);
+
+      while ((bytesRead = ifRead(serial, buffer, sizeof(buffer))))
+        processInput(serial, buffer, bytesRead);
+
+      pinReset(led);
+    }
+  }
+
+  return 0;
+}
